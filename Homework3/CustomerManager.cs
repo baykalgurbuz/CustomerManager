f6826b87f6c35cb97f9714b2c4eda4b68b5a2686
Enes Baykal <ebaykalgurbuz@gmail.com>
2021-01-20 13:47:24 +0300

    Add project files.

---
+++ Homework3/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework3
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine(customer.Name+" "+customer.Surname+" "+customer.Age);
+            Console.WriteLine("Addition is succesfully");
+        }
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " " + customer.Age);
+            Console.WriteLine("Deletion is succesfully");
+        }
+        public void CustomerList(Customer[] customers )
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("List 's results is : "+customer.Name+" "+customer.Surname+" "+customer.Age);
+            }
+        }
+
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.Id = 1;
+            c1.Name = "Baykal";
+            c1.Surname = "Gürbüz";
+            c1.Age = 21;
+
+            Customer c2 = new Customer();
+            c2.Id = 2;
+            c2.Name = "Ahmet";
+            c2.Surname = "Gürbüz";
+            c2.Age = 10;
+
+            Customer c3 = new Customer();
+            c3.Id = 3;
+            c3.Name = "Beyza";
+            c3.Surname = "Krcn";
+            c3.Age = 21;
+
+            Customer[] customers = new Customer[] {c1,c2,c3 };
+
+            CustomerManager cm1 = new CustomerManager();
+            cm1.CustomerAdd(c1);
+            cm1.CustomerDelete(c1);
+            cm1.CustomerAdd(c2);
+            cm1.CustomerDelete(c2);
+            cm1.CustomerAdd(c3);
+            cm1.CustomerDelete(c3);
+            cm1.CustomerList(customers);
+        }
+    }
+}
